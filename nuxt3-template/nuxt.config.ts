b56e3cae5468c8732e91aa8da5e8497837900ddd
Angel <angel_jadan@outlook.com>
2025-04-01 19:34:16 -0500

    init config spa

--- nuxt3-template/nuxt.config.ts
+++ nuxt3-template/nuxt.config.ts
@@ -1,6 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
+  srcDir: 'src',
+  ssr: false, // enable SPA mode
+  app: {
+    head: {
+      title: 'Nuxt App',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ]
+    }
+  }
 })
